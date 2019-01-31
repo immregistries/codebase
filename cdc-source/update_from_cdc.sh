52f8cfab70be8be79a2978aa7952a731b3a6d6fa
Chris Sorenson <sorech02@gmail.com>
2019-01-31 15:54:32 -0600

    Format the CDC files in place with xmllint
    With the idea of future updates being repeatable, use the standard
    utility "xmllint" to format the xml, since Eclipse would not be used
    by all parties.
    
    Also converted to use the line endings exactly as they download from
    the CDC website
    
    Simple script added to the directory, for a repeatable update process.

---
+++ cdc-source/update_from_cdc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Assumed to run from same directory
+
+#XML
+wget https://www2a.cdc.gov/vaccines/iis/iisstandards/XML.asp?rpt=cpt -O cpt.xml
+wget https://www2a.cdc.gov/vaccines/iis/iisstandards/XML2.asp?rpt=cvx -O cvx.xml
+wget https://www2a.cdc.gov/vaccines/iis/iisstandards/XML2.asp?rpt=cvxvis -O cvxvis.xml
+wget https://www2a.cdc.gov/vaccines/iis/iisstandards/XML.asp?rpt=tradename -O tradename.xml
+wget https://www2a.cdc.gov/vaccines/iis/iisstandards/XML.asp?rpt=vax2vg -O vac2vg.xml
+
+wget https://www2a.cdc.gov/vaccines/iis/iisstandards/VIS_xml.asp -O vis.xml
+wget https://www2a.cdc.gov/vaccines/iis/iisstandards/VISURL_xml.asp -O visurl.xml
+
+#FLAT FILES
+wget https://www2a.cdc.gov/vaccines/iis/iisstandards/downloads/NDC/NDC_Unit_use.txt -O NDC_Unit_use.txt
+wget https://www2a.cdc.gov/vaccines/iis/iisstandards/downloads/NDC/NDC_Unit_sale.txt -O NDC_Unit_sale.txt
+wget https://www2a.cdc.gov/vaccines/iis/iisstandards/downloads/NDC/NDC_Linker.txt -O NDC_Linker.txt
+
+for f in *.xml; do xmllint -o $f --format $f; done
