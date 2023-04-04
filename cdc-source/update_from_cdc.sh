a204865f705a51102cd76899e80cf04fb3245d94
Chris Sorenson <csorenson@immregistries.org>
2023-04-04 14:58:43 -0500

    Updates for April 4, 2023

--- cdc-source/update_from_cdc.sh
+++ cdc-source/update_from_cdc.sh
@@ -8,6 +8,7 @@ wget https://www2a.cdc.gov/vaccines/iis/iisstandards/XML2.asp?rpt=cvx -O cvx.xml
 wget https://www2a.cdc.gov/vaccines/iis/iisstandards/XML2.asp?rpt=cvxvis -O cvxvis.xml
 wget https://www2a.cdc.gov/vaccines/iis/iisstandards/XML.asp?rpt=tradename -O tradename.xml
 wget https://www2a.cdc.gov/vaccines/iis/iisstandards/XML.asp?rpt=vax2vg -O vac2vg.xml
+wget https://www2a.cdc.gov/vaccines/iis/iisstandards/XML.asp?rpt=mvx -O mvx.xml
 
 wget https://www2a.cdc.gov/vaccines/iis/iisstandards/VIS_xml.asp -O vis.xml
 wget https://www2a.cdc.gov/vaccines/iis/iisstandards/VISURL_xml.asp -O visurl.xml
