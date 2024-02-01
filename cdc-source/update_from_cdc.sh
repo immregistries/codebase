231b11037c53af92810dd4f18e2840c65290a973
Chris Sorenson <csorenson@immregistries.org>
2024-02-01 07:50:16 -0600

    February 01, 2024 updates

--- cdc-source/update_from_cdc.sh
+++ cdc-source/update_from_cdc.sh
@@ -2,7 +2,7 @@
 
 # Assumed to run from same directory
 
-#XML
+# XML
 wget https://www2a.cdc.gov/vaccines/iis/iisstandards/XML.asp?rpt=cpt -O cpt.xml
 wget https://www2a.cdc.gov/vaccines/iis/iisstandards/XML2.asp?rpt=cvx -O cvx.xml
 wget https://www2a.cdc.gov/vaccines/iis/iisstandards/XML2.asp?rpt=cvxvis -O cvxvis.xml
@@ -13,11 +13,12 @@ wget https://www2a.cdc.gov/vaccines/iis/iisstandards/XML.asp?rpt=mvx -O mvx.xml
 wget https://www2a.cdc.gov/vaccines/iis/iisstandards/VIS_xml.asp -O vis.xml
 wget https://www2a.cdc.gov/vaccines/iis/iisstandards/VISURL_xml.asp -O visurl.xml
 
-#FLAT FILES
+# FLAT FILES
 wget https://www2a.cdc.gov/vaccines/iis/iisstandards/downloads/NDC/NDC_Unit_use.txt -O NDC_Unit_use.txt
 wget https://www2a.cdc.gov/vaccines/iis/iisstandards/downloads/NDC/NDC_Unit_sale.txt -O NDC_Unit_sale.txt
 wget https://www2a.cdc.gov/vaccines/iis/iisstandards/downloads/NDC/NDC_Linker.txt -O NDC_Linker.txt
 
+# Perform linting on xml to ensure detected changes are significant
 for f in *.xml; do xmllint -o $f --format $f; done
 
 
